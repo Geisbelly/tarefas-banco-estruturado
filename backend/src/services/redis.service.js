70e37a1b44843406904aa50cc65c091aa8d0a9d5
Domiciano <guilhermedomicianosilva@gmail.com>
2025-06-04 13:35:43 -0300

    tentando fazer deploy no render

--- backend/src/services/redis.service.js
+++ backend/src/services/redis.service.js
@@ -1,12 +1,15 @@
 import { createClient } from "redis";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const client = createClient({
-  url: "redis://localhost:6379",
+  url: process.env.REDIS_URL,
 });
 
-client.on("error", (err) => {
-  console.error("Erro no Redis:", err);
+client.on('connect', () => {
+  console.log('✅ Conectado ao Redis com sucesso!');
 });
+
 
 await client.connect();
 
